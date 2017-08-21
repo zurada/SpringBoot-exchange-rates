2fcfe1c64e113ecd78582137a556f1c7dcf5be4d
azurada <adamzurada@gmail.com>
2017-08-21 08:33:54 +0200

    refactoring

--- src/main/java/com/avrios/sample/exchange/xml/CubeTimeAdapter.java
+++ src/main/java/com/avrios/sample/exchange/xml/CubeTimeAdapter.java
@@ -1,16 +1,9 @@
 package com.avrios.sample.exchange.xml;
 
-import lombok.extern.slf4j.Slf4j;
-import org.apache.tomcat.jni.Local;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.xml.bind.annotation.adapters.XmlAdapter;
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
-@Slf4j
 public class CubeTimeAdapter extends XmlAdapter<String, LocalDate> {
 
     private static final String DATE_FORMAT = "yyyy-MM-dd";
@@ -28,7 +21,7 @@ public class CubeTimeAdapter extends XmlAdapter<String, LocalDate> {
     @Override
     public LocalDate unmarshal(String time) throws Exception {
         if (time != null) {
-            return  LocalDate.parse(time,FORMATTER);
+            return LocalDate.parse(time, FORMATTER);
         }
         return null;
     }
